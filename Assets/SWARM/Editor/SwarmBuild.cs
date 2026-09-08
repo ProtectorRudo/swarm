cbd919a7faab00ffd3bd347bffc8da75e536f968
ProtectorRudo <mauroemilianodanielmorales@gmail.com>
2026-09-07 21:08:13 -0300

    chore: target 0.3 core rework APK

--- Assets/SWARM/Editor/SwarmBuild.cs
+++ Assets/SWARM/Editor/SwarmBuild.cs
@@ -10,7 +10,7 @@ namespace Swarm.Editor
 {
     public static class SwarmBuild
     {
-        private const string OutputPath = "Builds/Android/SWARM-0.2-first-test.apk";
+        private const string OutputPath = "Builds/Android/SWARM-0.3-core-rework.apk";
 
         [MenuItem("SWARM/Build Android APK")]
         public static void BuildAndroid()
@@ -50,7 +50,7 @@ namespace Swarm.Editor
 
             Debug.Log("SWARM_APK_READY=" + Path.GetFullPath(OutputPath));
             Debug.Log("SWARM_APK_SIZE_BYTES=" + report.summary.totalSize);
-            Debug.Log("SWARM_BUILD_VERSION=0.2-first-test");
+            Debug.Log("SWARM_BUILD_VERSION=0.3-core-rework");
         }
 
         private static void ValidateProject()
